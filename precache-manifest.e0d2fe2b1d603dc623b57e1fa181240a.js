3f491cd52074f3e164bc2234f9ebd3d2e3a660c0
Harshu53 <harshalmahajan53@gmail.com>
2019-08-29 21:28:23 +0530

    Updates

---
+++ precache-manifest.e0d2fe2b1d603dc623b57e1fa181240a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36abb88755da2c06e2d567c077385676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f13fc8e3f59e2be03bdb",
+    "url": "/static/css/main.beab340f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f13fc8e3f59e2be03bdb",
+    "url": "/static/js/main.607c7511.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
